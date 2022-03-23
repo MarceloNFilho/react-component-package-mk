77e48c3edf516ae023d01325096887056b5aab40
marcelonfilho <marcelonfilho.ms@gmail.com>
2022-03-23 15:58:06 -0400

    teste

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+declare const TestingComponent: React.FC;
+export default TestingComponent;
---
+++ packages/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const TestingComponent: React.FC = () => {
+  return <button>Salvar</button>
+}
+
+export default TestingComponent;
