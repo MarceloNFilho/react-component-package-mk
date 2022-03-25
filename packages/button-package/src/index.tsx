089f9f2bf31a5c97c57e1373b6ae72b2bdeca0d7
marcelonfilho <marcelonfilho.ms@gmail.com>
2022-03-25 10:11:14 -0400

    feat: add next/router

--- packages/button-package/src/index.tsx
+++ packages/button-package/src/index.tsx
@@ -1,14 +1,16 @@
 import axios from 'axios';
 import * as React from 'react';
+import { useRouter } from 'next/router';
 
 export const ButtonComponent: React.FC<{repoOwner: String}> = ({repoOwner}) => {
   
   console.log('repoOwner: ', repoOwner)
   async function callApi(repoOwner: String){
+    const router = useRouter();
     return await axios.get(`https://api.github.com/orgs/${repoOwner}/repos`)
     .then((res) => {
       console.log(res)
-      window.location.assign('www.google.com');
+      router.push('/home')
     })
   }
   return (
