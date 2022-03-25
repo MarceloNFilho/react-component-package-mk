442e60d73a998460d504528bbd93c4669c344c25
marcelonfilho <marcelonfilho.ms@gmail.com>
2022-03-25 11:38:02 -0400

    feat: update version

--- packages/button-package/src/index.tsx
+++ packages/button-package/src/index.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 import * as React from 'react';
 import { useRouter } from 'next/router';
 
@@ -7,34 +7,36 @@ import { useRouter } from 'next/router';
 //   router.push('/gluteo-esquerdo')
 // }
 
-const handleButtonClick = (repoOwner: String) => {
-  const handleSuccess = () => {
-    const router = useRouter();
-    router.push('/gluteo-direito')
-  }
-
-  return new Promise<String>(async resolve => {
-    return await axios.get(`https://api.github.com/orgs/${repoOwner}/repos`)
-    .then(res => {
-      alert(res.data);
-      handleSuccess()
-      resolve('');
-    })
-    // .catch(err => {
-    //   alert(err);
-    //   handleError()
-    //   resolve('');
-    // })
-  });
+const handleSuccess = () => {
+  const router = useRouter();
+  router.push('/gluteo-direito')
 }
+
+// const handleButtonClick = (repoOwner: String) => {
+
+//   return new Promise<String>(async resolve => {
+//     return await axios.get(`https://api.github.com/orgs/${repoOwner}/repos`)
+//     .then(res => {
+//       alert(res.data);
+//       handleSuccess()
+//       resolve('');
+//     })
+//     // .catch(err => {
+//     //   alert(err);
+//     //   handleError()
+//     //   resolve('');
+//     // })
+//   });
+// }
  
 export const ButtonComponent: React.FC<{repoOwner: String}> = ({repoOwner}) => {
+  console.log(repoOwner);
   return (
     <>
       <span>
         Teste N° 375647356758678787867999999
       </span>
-      <button onClick={() => handleButtonClick(repoOwner)}>
+      <button onClick={() => handleSuccess()}>
         DESGRAÇAAAAA
       </button>
     </>
